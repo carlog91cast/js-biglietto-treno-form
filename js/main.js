04202f1dbef7e169816c80a36839fea477481e46
bigeppo <carlog91cast@gmail.com>
2022-06-15 18:38:37 +0200

    add milestone 2

--- js/main.js
+++ js/main.js
@@ -7,11 +7,17 @@
 let btnGenera = document.getElementById('btn-gnr');
 btnGenera.addEventListener('click', function () {
 
+    // input name
+
+    let inputName = document.getElementById("user-name");
+    let userName = inputName.value;
+    console.log(userName);
+
     // imput age
 
     let inputAge = document.getElementById("user-age");
-    let Age = inputAge.value;
-    console.log(Age);
+    let age = inputAge.value;
+    console.log(age);
 
     // imput km
 
@@ -22,30 +28,28 @@ btnGenera.addEventListener('click', function () {
     // calcolo distanza-prezzo
 
     let costoAlKm = 0.21;
-    let ticketPrice = userDistance * costoAlKm;
-    console.log(prezzoSenzaSconto)
-
-
-    if (userAge < 18) {
-        console.log('è minorenne');
-        ticketPrice = ticketPrice * 80 / 100;
-    } else if (userAge > 65) {
-        console.log('over 65');
-        ticketPrice = ticketPrice * 60 / 100;
-    }
-    let num = ticketPrice;
-    let n = num.toFixed(2);
+    let ticketPrice = percorrenza * costoAlKm;
     console.log(ticketPrice);
-    document.getElementById('prezzo-scontato').innerHTML = ticketPrice;
+
+
+    if (age < 18) {
+        console.log('è minorenne');
+        ticketPrice = ticketPrice * 85 / 100;
+    } else if (age > 65) {
+        console.log('over 65');
+        ticketPrice = ticketPrice * 65 / 100;
+    }
+
+    document.getElementById('passenger').innerHTML = userName;
+    document.getElementById('ticket-cost').innerHTML = ticketPrice.toFixed(2);
+
 
 
 
 });
 
-let num = ticketPrice;
-let n = num.toFixed(2);
-console.log(ticketPrice);
-document.getElementById('prezzo-scontato').innerHTML = ticketPrice;
+
+
 
 // ora calcolo il prezzo totale del viaggio
 
