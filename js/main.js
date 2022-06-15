8975d09038c259f803725a044ae149ff3b0e4d6e
bigeppo <carlog91cast@gmail.com>
2022-06-15 17:31:09 +0200

    add input function

--- js/main.js
+++ js/main.js
@@ -1,12 +1,45 @@
 //Il programma che dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero o della passeggera.
 //  chiedo all'utente quanti km vuole percorrere
 
-const userDistance = prompt('inserisci la percorrenza in km');
-document.getElementById('go-to').innerHTML = userDistance;
+
 // chiedo all'utente la sua età
 
-const userAge = parseInt(prompt('inserisci la tua età'))
-document.getElementById('age').innerHTML = userAge;
+let btnGenera = document.getElementById('btn-gnr');
+btnGenera.addEventListener('click', function () {
+
+    // imput age
+
+    let inputAge = document.getElementById("user-age");
+    let Age = inputAge.value;
+    console.log(Age);
+
+    // imput km
+
+    let inputKm = document.getElementById("km");
+    let percorrenza = parseInt(inputKm.value);
+    console.log(percorrenza);
+
+    // calcolo distanza-prezzo
+
+    let costoAlKm = 0.21;
+    let ticketPrice = userDistance * costoAlKm;
+
+
+    if (userAge < 18) {
+        console.log('è minorenne');
+        ticketPrice = ticketPrice * 80 / 100;
+    } else if (userAge > 65) {
+        console.log('over 65');
+        ticketPrice = ticketPrice * 60 / 100;
+    }
+    let num = ticketPrice;
+    let n = num.toFixed(2);
+    console.log(ticketPrice);
+    document.getElementById('prezzo-scontato').innerHTML = ticketPrice;
+
+
+
+});
 
 // ora calcolo il prezzo totale del viaggio
 
@@ -27,18 +60,6 @@ document.getElementById('prezzo-senza-sconto').innerHTML = ticketPrice;
 
 // definisco in base all'età dell'utente se esso sia maggiorenne o over 65
 // va applicato poi uno sconto del 20% per i minorenni e del 40% per gli over 65.
-
-if (userAge < 18) {
-    console.log('è minorenne');
-    ticketPrice = ticketPrice * 80 / 100;
-} else if (userAge > 65) {
-    console.log('over 65');
-    ticketPrice = ticketPrice * 60 / 100;
-}
-let num = ticketPrice;
-let n = num.toFixed(2);
-console.log(ticketPrice);
-document.getElementById('prezzo-scontato').innerHTML = ticketPrice;
 
 
 
